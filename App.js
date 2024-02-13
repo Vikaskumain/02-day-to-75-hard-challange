0b220fac08ecd505028edfc3ea80e57f2782a9b0
Vikas Chand <121432906+Vikaskumain@users.noreply.github.com>
2024-02-13 14:56:43 +0530

    Episode-no-02

---
+++ App.js
@@ -0,0 +1,5 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+const heading1=React.createElement("h1",{id:"vikas"},"hello React.js and learning in core fundamtal")
+const root =ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading1)
